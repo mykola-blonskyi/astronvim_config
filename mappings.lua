d75d0a2ac0ca0cce99f79bf94fb5e147f4cee43a
Mykola Blonkyi <nikolay.blonskiy@gmail.com>
2023-05-10 18:37:43 +0300

    Bind to F2 relative path copy

--- mappings.lua
+++ mappings.lua
@@ -22,6 +22,14 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<F2>"] = {
+      function()
+        local path = vim.fn.expand "%:."
+        vim.fn.setreg("@+", path)
+        vim.notify('Copied "' .. path .. '" to the clipboard!')
+      end,
+      desc = "Some test desc",
+    },
   },
   t = {
     -- setting a mapping to false will disable it
