5277fbfebc0ab7f0c48ab4f26225e99fedcc9ddf
Mykola Blonkyi <nikolay.blonskiy@gmail.com>
2023-06-08 19:15:02 +0300

    Updated plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,7 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.file-explorer.oil-nvim" },
+  { import = "astrocommunity.bars-and-lines.lualine-nvim" },
+  { import = "astrocommunity.utility.transparent-nvim" },
 }
